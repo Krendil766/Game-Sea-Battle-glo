5554552d870edae72ba6fe7bd6a1fb6e4ee1491f
Krendil766 <krendil766@gmail.com>
2020-11-04 23:53:19 +0300

    Adde generate coordinates ships

--- script.js
+++ script.js
@@ -6,27 +6,50 @@ const enemy = document.querySelector('#enemy');
 const again = document.querySelector('#again');
 const header = document.querySelector('.header')
 
-const game ={
-    ships: [
-        {
-            location: ['26', '36', '46', '56'],
-            hit: ['', '', '', '']
-        },
-        {
-            location: ['11', '12', '13'],
-            hit: ['', '', '']
-        },
-        {
-            location: ['69', '79'],
-            hit: ['', '']
-        },
-        {
-            location: ['32'],
-            hit: ['']
-        },
-    ],
-    shipCount: 4,
-}
+const game = {
+    ships: [],
+    shipCount: 0,
+    optionShips: {
+        count: [1, 2, 3, 4],
+        size: [4, 3, 2, 1],
+    },
+    generateShip() {
+        for (let i = 0; i < this.optionShips.count.length; i++) {
+            for (let j = 0; j < this.optionShips.count[i]; j++) {
+                const size = this.optionShips.size[i];
+                const ship = this.generateOptionsShip(size);
+                this.ships.push(ship);
+                this.shipCount++;
+            }
+        }
+    },
+    generateOptionsShip(shipSize) {
+        const ship = {
+            hit: [],
+            location: [],
+        };
+        let x, y;
+
+        const direction = Math.random() < 0.5;
+        if (direction) {
+            x = Math.floor(Math.random() * 10);
+            y = Math.floor(Math.random() * (10 - shipSize));
+        } else {
+            x = Math.floor(Math.random() * (10 - shipSize));
+            y = Math.floor(Math.random() * 10);
+        };
+
+        for (let i = 0; i < shipSize; i++) {
+            if (direction) {
+                ship.location.push(x + "" + (y + i));
+            } else {
+                ship.location.push((x + i) + "" + y);
+            }
+            ship.hit.push('');
+        }
+        return ship;
+    },
+};
 
 const play = {
     record: localStorage.getItem('seaBattleRecord') || 0,
@@ -62,31 +85,31 @@ const show = {
 
 const fire = (e) => {
     const target = e.target;
-    if(target.classList.length!==0 || target.tagName!=='TD' || game.shipCount<1)return;
+    if (target.classList.length !== 0 || target.tagName !== 'TD' || !game.shipCount) return;
     play.updateData = 'shot';
     show.miss(target);
 
-    for(let i=0;i<game.ships.length;i++){
+    for (let i = 0; i < game.ships.length; i++) {
         const ship = game.ships[i];
         const index = ship.location.indexOf(target.id);
-        if(index>=0){
+        if (index >= 0) {
             show.hit(target);
             play.updateData = 'hit';
             ship.hit[index] = 'xxx';
             const life = ship.hit.indexOf('');
-            if(life==-1){
+            if (life == -1) {
                 play.updateData = 'dead';
-                for(const id of ship.location){
+                for (const id of ship.location) {
                     show.dead(document.getElementById(id))
                 };
                 game.shipCount--;
-                if(game.shipCount<1){
-                    header.textContent='GAME OVER';
+                if (!game.shipCount) {
+                    header.textContent = 'GAME OVER';
                     header.style.color = 'red';
                     target.classList.remove('shot')
-                    if(play.shot<play.record || play.record===0){
+                    if (play.shot < play.record || play.record === 0) {
                         localStorage.setItem('seaBattleRecord', play.shot);
-                        play.record=play.shot;
+                        play.record = play.shot;
                         play.render();
                     }
 
@@ -100,9 +123,17 @@ const fire = (e) => {
 const init = () => {
     enemy.addEventListener('click', fire);
     play.render();
+    game.generateShip();
 
-    again.addEventListener('click',()=>{
+    console.log(game.ships);
+
+    again.addEventListener('click', () => {
         location.reload();
-    })
+    });
+    record.addEventListener('dblclick', () => {
+        localStorage.clear();
+        play.record = 0;
+        play.render();
+    });
 }
 init();
